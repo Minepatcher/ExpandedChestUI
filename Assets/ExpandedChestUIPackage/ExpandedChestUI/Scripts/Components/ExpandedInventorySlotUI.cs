6cb03c972be5a3a671c35e7bca29f0c0cc654c2d
Minepatcher <theminepatcher@gmail.com>
2025-07-11 13:04:37 -0600

    Patch Fix
    - Fix: When focused on the chest inventory. The out-of-bounds slots will sometimes cover the player inventory slots.

--- Assets/ExpandedChestUIPackage/ExpandedChestUI/Scripts/Components/ExpandedInventorySlotUI.cs
+++ Assets/ExpandedChestUIPackage/ExpandedChestUI/Scripts/Components/ExpandedInventorySlotUI.cs
@@ -7,7 +7,7 @@ namespace ExpandedChestUI.Scripts.Components
         public override float localScrollPosition => transform.localPosition.y + transform.parent.localPosition.y;
         
         // ReSharper disable once InconsistentNaming
-        private bool showHoverWindow => slotsUIContainer is not null && slotsUIContainer.scrollWindow.IsShowingPosition(localScrollPosition, background.size.y);
+        private bool showHoverWindow => IsMouseWithinScrollArea() && slotsUIContainer is not null && slotsUIContainer.scrollWindow.IsShowingPosition(localScrollPosition, background.size.y);
 
         public override bool isVisibleOnScreen => showHoverWindow && isActiveAndEnabled && transform.lossyScale.x != 0.0 && transform.lossyScale.y != 0.0;
 
@@ -17,6 +17,15 @@ namespace ExpandedChestUI.Scripts.Components
         {
             slotsUIContainer?.scrollWindow?.MoveScrollToIncludePosition(localScrollPosition, background.size.y / 2f);
             OnSelectSlot();
+        }
+        
+        private bool IsMouseWithinScrollArea()
+        {
+            Vector3 position1 = Manager.ui.mouse.pointer.transform.position;
+            Vector2 size = new Vector2(uiScrollWindow.windowWidth, uiScrollWindow.windowHeight);
+            Vector2 vector2 = new Vector2(uiScrollWindow.windowWidth, uiScrollWindow.windowHeight) / 2f;
+            Vector3 position2 = uiScrollWindow.transform.position;
+            return new Rect(new Vector2(position2.x, position2.y) + uiScrollWindow.windowLocalCenter - vector2, size).Contains(position1);
         }
     }
     
